f6bfd84cd4f2b2a383e47abc97157045f4528ac5
rajkumar <rajkumar.podila35@gmail.com>
2021-01-28 16:12:34 +0530

    Changes done according to the req

--- demo2/demo2/ViewController.swift
+++ demo2/demo2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("Dinchak! Dinchak!")
+        print("Wel come to the Github")
     }
 
 
